a4fed3f56961936a5d92584ce186683d9ba14a07
Felipe DR <felipe.corp7@gmail.com>
2025-04-06 15:17:27 -0300

    feat(app): implements landing page

---
+++ src/app/(landing-page)/page.tsx
@@ -0,0 +1,37 @@
+import { JSX } from 'react';
+
+import {
+  AboutComponent,
+  EcologicComponent,
+  HeroComponent,
+  HeroHeaderComponent,
+  RankingComponent,
+  StatsDonationsComponent,
+  SupportComponent,
+  TitleComponent,
+} from '@/components';
+
+export default function LandingPage(): JSX.Element {
+  return (
+    <>
+      <HeroComponent>
+        <HeroHeaderComponent>
+          <TitleComponent
+            className="animate-fadein text-base-white opacity-0 md:ms-8 lg:ms-11"
+            tag="h1"
+          >
+            <span className="text-primary-2">Doação</span> que educa, atitude
+            que <span className="text-primary-2">preserva</span>
+          </TitleComponent>
+        </HeroHeaderComponent>
+      </HeroComponent>
+      <main>
+        <AboutComponent />
+        <RankingComponent />
+        <StatsDonationsComponent />
+        <SupportComponent />
+        <EcologicComponent />
+      </main>
+    </>
+  );
+}
