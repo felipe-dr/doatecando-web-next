faa9815af3e28fa5703f37a8d3afda1b51aa9821
Felipe DR <felipe.corp7@gmail.com>
2025-04-09 20:29:54 -0300

    feat(app): implements DonationPage

---
+++ src/app/(landing-page)/doacao/page.tsx
@@ -0,0 +1,44 @@
+import { getSchools } from '@/http';
+
+import {
+  HeroComponent,
+  HeroHeaderComponent,
+  SchoolsComponent,
+  SectionBoxComponent,
+  TitleComponent,
+} from '@/components';
+
+export default async function DonationPage(): Promise<JSX.Element | undefined> {
+  const schools = await getSchools();
+
+  return (
+    <>
+      <HeroComponent>
+        <HeroHeaderComponent>
+          <TitleComponent
+            className="animate-fadein text-base-white opacity-0 md:ms-8 lg:ms-11"
+            tag="h1"
+          >
+            Sua <span className="text-primary-2">doação</span> renova a{' '}
+            <span className="text-primary-2">educação</span>
+          </TitleComponent>
+        </HeroHeaderComponent>
+      </HeroComponent>
+      <main>
+        <SectionBoxComponent
+          className="pb-0 lg:pb-0"
+          hasContainer={false}
+          tag="section"
+        >
+          <div className="container max-w-screen-xl">
+            {schools?.length ? (
+              <SchoolsComponent schools={schools} />
+            ) : (
+              <p>Não há escolas disponíveis no momento.</p>
+            )}
+          </div>
+        </SectionBoxComponent>
+      </main>
+    </>
+  );
+}
