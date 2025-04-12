3fe00a9d3ec59b4f5339161229207e94b0c1d42c
Felipe DR <felipe.corp7@gmail.com>
2025-04-12 11:23:13 -0300

    feat(http): add page and limit

--- src/http/get-schools/get-schools.http.ts
+++ src/http/get-schools/get-schools.http.ts
@@ -2,10 +2,23 @@ import { SchoolModel } from '@/shared/models';
 
 import { API_URL } from '../api-client/api-client.http';
 
-type GetSchoolsHttpResponse = SchoolModel[] | undefined;
+type GetSchoolsHttpRequest = {
+  page?: number;
+  limit?: number;
+};
 
-export async function getSchools(): Promise<GetSchoolsHttpResponse> {
-  const response = await fetch(`${API_URL}/schools`);
+type GetSchoolsHttpResponse = SchoolModel[];
+
+export async function getSchools({
+  page = 1,
+  limit = 10,
+}: GetSchoolsHttpRequest = {}): Promise<GetSchoolsHttpResponse> {
+  const response = await fetch(
+    `${API_URL}/schools?page=${page}&limit=${limit}`,
+  );
+
+  if (!response.ok) return [];
+
   const schools: SchoolModel[] = await response.json();
 
   return schools;
