bb32671c4ca61a6044cdc4f62c30622d7cf8ac8e
Felipe DR <felipe.corp7@gmail.com>
2025-04-11 21:42:43 -0300

    feat(http): implements getDonatedItemsHttp

--- src/http/get-donated-items/get-donated-items.http.ts
+++ src/http/get-donated-items/get-donated-items.http.ts
@@ -2,10 +2,18 @@ import { DonatedItemModel } from '@/shared/models';
 
 import { API_URL } from '../api-client/api-client.http';
 
+type GetDonatedItemsHttpRequest = {
+  page?: number;
+  limit?: number;
+};
+
 type GetDonatedItemsHttpResponse = DonatedItemModel[] | undefined;
 
-export async function getDonatedItemsHttp(): Promise<GetDonatedItemsHttpResponse> {
-  const response = await fetch(`${API_URL}/items`);
+export async function getDonatedItemsHttp({
+  page = 1,
+  limit = 10,
+}: GetDonatedItemsHttpRequest = {}): Promise<GetDonatedItemsHttpResponse> {
+  const response = await fetch(`${API_URL}/items?page=${page}&limit=${limit}`);
   const donatedItems: DonatedItemModel[] = await response.json();
 
   return donatedItems;
