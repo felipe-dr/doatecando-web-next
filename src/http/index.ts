236e697c84d3d2f06132e885af57541077b978e5
Felipe DR <felipe.corp7@gmail.com>
2025-04-13 16:11:29 -0300

    feat: add imports

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,4 +1,7 @@
 export * from './api-client/api-client.http';
+export * from './create-donation/create-donation.http';
 export * from './get-donated-items/get-donated-items.http';
+export * from './get-donors/get-donors.http';
 export * from './get-schools/get-schools.http';
 export * from './signin/signin.http';
+export * from './signup/signup.http';
