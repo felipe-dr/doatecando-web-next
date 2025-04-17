d0890532be3578e8206291ba72da7fdf932bac78
Felipe DR <felipe.corp7@gmail.com>
2025-04-16 23:08:43 -0300

    feat(app): add security

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,3 +1,5 @@
+import escapeHtml from 'escape-html';
+import { revalidatePath } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
@@ -7,6 +9,8 @@ export async function POST(request: Request) {
     street,
     number,
     postalCode,
+    latitude,
+    longitude,
     neighbourhood,
     unprivilegedArea,
     urgency,
@@ -17,31 +21,37 @@ export async function POST(request: Request) {
     password,
   } = await request.json();
 
+  const sanitizedSignupForm = {
+    name: escapeHtml(name),
+    street: escapeHtml(street),
+    number: escapeHtml(number),
+    postalCode: postalCode,
+    latitude: latitude,
+    longitude: longitude,
+    neighbourhood: escapeHtml(neighbourhood),
+    unprivilegedArea,
+    urgency,
+    quantityOfStudents: quantityOfStudents,
+    availability,
+    phone: phone,
+    email: escapeHtml(email),
+    password: password,
+  };
+
   try {
     const response = await fetch(`${API_URL}/schools`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        name,
-        street,
-        number,
-        postalCode,
-        neighbourhood,
-        unprivilegedArea,
-        urgency,
-        quantityOfStudents,
-        availability,
-        phone,
-        email,
-        password,
-      }),
+      body: JSON.stringify(sanitizedSignupForm),
     });
 
     if (!response.ok) {
       return response;
     }
+
+    revalidatePath('/doacao');
 
     const data = await response.json();
     const nextResponse = NextResponse.json({
