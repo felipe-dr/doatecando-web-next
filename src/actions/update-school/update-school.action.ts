066a5040dec6252df2d511dbed5c42290a7ff63a
Felipe DR <felipe.corp7@gmail.com>
2025-04-18 15:07:23 -0300

    feat(actions): implements actions

---
+++ src/actions/update-school/update-school.action.ts
@@ -0,0 +1,50 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+
+import { updateSchoolHttp } from '@/http';
+
+import { signupDialogSchema } from '@/components';
+
+export async function updateSchoolAction(formData: FormData) {
+  const formDataObject = Object.fromEntries(formData.entries());
+  const schoolSchemaValidation = signupDialogSchema.safeParse(formDataObject);
+  const school = schoolSchemaValidation.data!;
+
+  if (!schoolSchemaValidation.success) {
+    const errors = schoolSchemaValidation.error.flatten().fieldErrors;
+
+    return { success: false, message: 'Erro na validação de dados', errors };
+  }
+
+  try {
+    const accessToken = cookies().get('accessToken')?.value;
+
+    if (!accessToken) {
+      return {
+        message: 'Um erro inesperado ocorreu!',
+      };
+    }
+
+    const response = await updateSchoolHttp({
+      accessToken,
+      school,
+    });
+
+    if (response?.error) {
+      return { success: false, message: response?.error };
+    }
+
+    revalidatePath('/admin/donors');
+    revalidatePath('/admin/donors/add');
+    revalidatePath('/admin/donations');
+    revalidatePath('/admin/donations/add');
+    revalidatePath('/admin/schools');
+    revalidatePath('/');
+
+    return { success: true, message: 'sucesso' };
+  } catch (error) {
+    return { success: false, message: null, error };
+  }
+}
